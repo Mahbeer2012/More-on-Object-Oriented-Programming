3497d5bfaaeb5a6e386da8340c2fb458651e26ef
Mahbeer2012 <mahbeermubashir031@gmail.com>
2025-04-25 14:52:04 +0500

    Activity1.py

---
+++ Activity1.py
@@ -0,0 +1,12 @@
+# create class
+class IOString():
+
+    # constructor to set default
+ def __init__(self):
+  self.str1 =""
+
+  # function to get input from user
+def get_string(self):
+ self.str1 = input("enter string  :")
+
+ 
